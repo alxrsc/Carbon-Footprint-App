e029830405e9c611551376762646f0b85450f025
Alexandru Rosca <alexandrurosca968@gmail.com>
2024-11-12 00:29:18 +0200

    Experimenting with a language select window

--- main.cpp
+++ main.cpp
@@ -1,38 +1,81 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QObject>
+
+class MainPage : public QWidget {
+    // Required for QObject-derived classes using signals/slots
+
+public:
+    QPushButton *helloButton;
+
+    MainPage(QWidget *parent = nullptr) : QWidget(parent) {
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        helloButton = new QPushButton("Hello", this);
+        layout->addWidget(helloButton);
+        setLayout(layout);
+        setWindowTitle("Main Page");
+        resize(320, 240);
+    }
+};
+
+class SelectLanguagePage : public QWidget {
+
+public:
+    QPushButton *englishButton;
+    QPushButton *romanianButton;
+    QPushButton *spanishButton;
+
+    SelectLanguagePage(QWidget *parent = nullptr) : QWidget(parent) {
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        englishButton = new QPushButton("English", this);
+        romanianButton = new QPushButton("Romanian", this);
+        spanishButton = new QPushButton("Spanish", this);
+        layout->addWidget(englishButton);
+        layout->addWidget(romanianButton);
+        layout->addWidget(spanishButton);
+        setLayout(layout);
+        setWindowTitle("Select Language");
+        resize(320, 240);
+    }
+};
+
+class MainController : public QObject {
+
+public:
+    MainPage mainPage;
+    SelectLanguagePage languagePage;
+
+    MainController() {
+        // Connect buttons to handle navigation between pages
+        QObject::connect(mainPage.helloButton, &QPushButton::clicked, this, &MainController::showLanguagePage);
+        QObject::connect(languagePage.englishButton, &QPushButton::clicked, this, &MainController::backToMainPage);
+        QObject::connect(languagePage.romanianButton, &QPushButton::clicked, this, &MainController::backToMainPage);
+        QObject::connect(languagePage.spanishButton, &QPushButton::clicked, this, &MainController::backToMainPage);
+    }
+
+    void showMainPage() {
+        languagePage.hide();
+        mainPage.show();
+    }
+
+    void showLanguagePage() {
+        mainPage.hide();
+        languagePage.show();
+    }
+
+    void backToMainPage() {
+        languagePage.hide();
+        mainPage.show();
+    }
+};
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    QWidget window;
-    QPushButton *button = new QPushButton("Hello World", &window);
-    window.resize(320, 240);
-    window.setWindowTitle("Carbon Footprint App");
-//    button->setGeometry(100, 100, 120, 30);
-    button->move(100, 100);
-    // create a new internal window when the button is pressed with a back button to the main page
-    QPushButton *backButton = new QPushButton("Back", &window);
-    backButton->move(10,10);
-    backButton->hide();
+    MainController controller;
+    controller.showMainPage();  // Start by showing the main page
 
-    QObject::connect(button, &QPushButton::clicked, [&]() {
-        button->hide();
-        backButton->show();
-    });
-
-    QObject::connect(backButton, &QPushButton::clicked, [&]() {
-        button->show();
-        backButton->hide();
-    });
-
-
-
-
-    window.show();
-
-// Maria: a mers.
-// Petrisor: https://www.youtube.com/watch?v=qf_8dyLdT5s
-// Andu: https://www.instagram.com/daniel.cdi/
     return app.exec();
 }
