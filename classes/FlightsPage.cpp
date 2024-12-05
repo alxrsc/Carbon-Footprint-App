0904f3dc3115a24abaf49ad7420aab423ceaee4f
Alexandru Rosca <alexandrurosca968@gmail.com>
2024-12-06 01:15:38 +0200

    Added a raw version of the Flights Page, still on the runway :D

---
+++ classes/FlightsPage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexandru Roșca on 06.12.2024.
+//
+
+#include "FlightsPage.h"
---
+++ classes/FlightsPage.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexandru Roșca on 06.12.2024.
+//
+
+#ifndef CARBONFOOTPRINTAPP_FLIGHTSPAGE_H
+#define CARBONFOOTPRINTAPP_FLIGHTSPAGE_H
+
+
+class FlightsPage {
+
+};
+
+
+#endif //CARBONFOOTPRINTAPP_FLIGHTSPAGE_H
