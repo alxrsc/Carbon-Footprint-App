b69fa0bd40c2ebbd3a4ce1e2dc1f56abe30b7b3f
Alexandru Rosca <alexandrurosca968@gmail.com>
2025-01-29 17:06:41 +0200

    removed leftover code from obsolete version

--- classes/FootprintMethodPage.cpp
+++ classes/FootprintMethodPage.cpp
@@ -17,34 +17,34 @@ void FootprintMethodPage::setupUI() {
         layout->addWidget(button);
     }
 
-    // Input form (hidden initially)
-    promptLabel = new QLabel("Enter the number of people:", this);
-    inputField = new QLineEdit(this);
-    submitButton = new QPushButton("Submit", this);
-
-    promptLabel->setStyleSheet(INSTRUCTION_STYLE);
-    inputField->setStyleSheet(INPUT_STYLE);
-    submitButton->setStyleSheet(BUTTON_STYLE);
-
-    layout->addWidget(promptLabel);
-    layout->addWidget(inputField);
-    layout->addWidget(submitButton);
-
-    promptLabel->hide();
-    inputField->hide();
-    submitButton->hide();
-
-    // Initialize form layout and container
-    formLayout = new QFormLayout;
-    formLayout->addRow(new QLabel("Electricity (kWh):", this), new QLineEdit(this));
-    formLayout->addRow(new QLabel("Petrol (litres):", this), new QLineEdit(this));
-    formLayout->addRow(new QLabel("Natural Gas (metric tons):", this), new QLineEdit(this));
-
-    formContainer = new QWidget(this); // Initialize form container
-    formContainer->setLayout(formLayout);
-    formContainer->hide(); // Initially hidden
-
-    layout->addWidget(formContainer);
+//    // Input form (hidden initially)
+//    promptLabel = new QLabel("Enter the number of people:", this);
+//    inputField = new QLineEdit(this);
+//    submitButton = new QPushButton("Submit", this);
+//
+//    promptLabel->setStyleSheet(INSTRUCTION_STYLE);
+//    inputField->setStyleSheet(INPUT_STYLE);
+//    submitButton->setStyleSheet(BUTTON_STYLE);
+//
+//    layout->addWidget(promptLabel);
+//    layout->addWidget(inputField);
+//    layout->addWidget(submitButton);
+//
+//    promptLabel->hide();
+//    inputField->hide();
+//    submitButton->hide();
+//
+//    // Initialize form layout and container
+//    formLayout = new QFormLayout;
+//    formLayout->addRow(new QLabel("Electricity (kWh):", this), new QLineEdit(this));
+//    formLayout->addRow(new QLabel("Petrol (litres):", this), new QLineEdit(this));
+//    formLayout->addRow(new QLabel("Natural Gas (metric tons):", this), new QLineEdit(this));
+//
+//    formContainer = new QWidget(this); // Initialize form container
+//    formContainer->setLayout(formLayout);
+//    formContainer->hide(); // Initially hidden
+//
+//    layout->addWidget(formContainer);
     setLayout(layout);
 
     setStyleSheet(GLOBAL_BACKGROUND_STYLE);
