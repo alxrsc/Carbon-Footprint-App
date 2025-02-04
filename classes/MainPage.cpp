c1fdc01c887740ae86786221b2f0b10430993098
maria-baba <surugium514@gmail.com>
2025-02-04 16:19:01 +0200

    translated credits to English

--- classes/MainPage.cpp
+++ classes/MainPage.cpp
@@ -17,22 +17,23 @@ void MainPage::setupUI() {
 
     infoLabel = new QLabel(this);
     infoLabel->setText(
-        "\"Gender, Digitalization, Green: Ensuring a Sustainable Future for all in Europe\"\n"
-        "Ref. Project: 2023-1-RO01-KA220-HED-000154433, lider: Universitatea de Stiinte Agricole si Medicina Veterinara\n"
-        "in care UNSTPB este partener, alaturi de: Universitat Autonoma de Barcelona, Spania,\n"
-        "Universidade do Porto, Portugalia si Uzhgorodskyi Nacionalnyi Universitet, Ukraina"
+    "\"Gender, Digitalization, Green: Ensuring a Sustainable Future for all in Europe\"\n"
+    "Ref. Project: 2023-1-RO01-KA220-HED-000154433, leader: University of Agricultural Sciences and Veterinary Medicine\n"
+    "in which UNSTPB is a partner, alongside: Universitat Autònoma de Barcelona, Spain,\n"
+    "University of Porto, Portugal, and Uzhhorod National University, Ukraine"
+
     );
     infoLabel->setWordWrap(true);  // Enable word wrapping
     infoLabel->setAlignment(Qt::AlignCenter);
     infoLabel->setStyleSheet("font-size: 16px; padding: 10px;");
 
     creditsLabel = new QLabel(this);
-    creditsLabel->setText("Team members:\n"
+    creditsLabel->setText("Team members:\n\n"
                           "- leader - Alexandru Roșca (alexandrurosca968@gmail.com)\n"
                           "- Maria-Beatrice Surugiu (surugium514@gmail.com)\n"
                           "- Petrișor-Andrei Leu (leupetrisorandrei.bd@gmail.com)\n"
                           "- Andu Bădescu (badescuandu2@gmail.com)\n\n"
-                          "Coordinating teachers:\n"
+                          "Coordinating teachers:\n\n"
                           "- Mihai Caramihai\n"
                           "- Daniel Chiș");
     creditsLabel->setWordWrap(true);
